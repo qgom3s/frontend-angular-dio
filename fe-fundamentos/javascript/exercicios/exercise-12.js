cfb7f35b811e62512b22fd0b3df7627edbec82ea
Quenã Gomes <81332306+qgom3s@users.noreply.github.com>
2024-01-10 11:36:54 -0300

    Update exercise-12.js
    Added classificação.

--- fe-fundamentos/javascript/exercicios/exercise-12.js
+++ fe-fundamentos/javascript/exercicios/exercise-12.js
@@ -13,8 +13,27 @@ class People {
         console.log(`Name: ${this.name}, height: ${this.height} and weight: ${this.weight}.`);
     }
 
-    imc() {
+    calcImc() {
         return this.weight / this.height ** 2;
+    }
+
+    classifyImc() {
+        const imc = this.calcImc();
+
+        if (imc < 18.5) {
+            return 'low weight';
+        } else if (imc < 25) {
+            return 'normal weight';
+        } else if (imc < 30) {
+            return 'overweight';
+        } else if (imc < 35) {
+            return 'obesity: grade I';
+        } else if (imc < 40) {
+            return 'obesity: grade II';
+        } else {
+            return 'obesity: grade III';
+        }
+
     }
 }
 
@@ -22,4 +41,5 @@ const jose = new People('José', 1.70, 70);
 
 jose.describe();
 
-console.log(`Jose IMC: ` + jose.imc().toFixed(2));
+console.log(`Jose IMC: ` + jose.calcImc().toFixed(2));
+console.log(`Body condition: ` + jose.classifyImc() + '.');
